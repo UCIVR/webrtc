cdc76d450cbe3b274708b72268efff4c8bc349a6
David Detweiler <daviddetweiler1@gmail.com>
2023-04-18 02:08:45 -0700

    Implement SDP handshake

--- examples/receiver/main.cc
+++ examples/receiver/main.cc
@@ -2,15 +2,110 @@
 #include <api/peer_connection_interface.h>
 #include <rtc_base/thread.h>
 
+#define BOOST_ALL_NO_LIB
+#include <boost/json.hpp>
+#include <boost/json/src.hpp>
 #include <iostream>
 
 #include "api/audio_codecs/builtin_audio_decoder_factory.h"
 #include "api/audio_codecs/builtin_audio_encoder_factory.h"
+#include "api/jsep.h"
 #include "api/video_codecs/builtin_video_decoder_factory.h"
 #include "api/video_codecs/builtin_video_encoder_factory.h"
+#include "websocketpp/config/asio_no_tls.hpp"
+#include "websocketpp/server.hpp"
+
+using server_type = websocketpp::server<websocketpp::config::asio>;
+using message_ptr = server_type::message_ptr;
+using websocketpp::lib::bind;
+using websocketpp::lib::placeholders::_1;
+using websocketpp::lib::placeholders::_2;
 
 namespace receiver {
-class Observer : public webrtc::PeerConnectionObserver {
+class observer : public webrtc::PeerConnectionObserver,
+                 public webrtc::SetSessionDescriptionObserver,
+                 public webrtc::CreateSessionDescriptionObserver {
+ public:
+  void start_signal_server() {
+    const auto signaling_thread = rtc::Thread::CreateWithSocketServer();
+    signaling_thread->Start();
+    const auto pc_factory = webrtc::CreatePeerConnectionFactory(
+        nullptr, nullptr, signaling_thread.get(), nullptr,
+        webrtc::CreateBuiltinAudioEncoderFactory(),
+        webrtc::CreateBuiltinAudioDecoderFactory(),
+        webrtc::CreateBuiltinVideoEncoderFactory(),
+        webrtc::CreateBuiltinVideoDecoderFactory(), nullptr, nullptr);
+
+    if (!pc_factory) {
+      std::cerr << "Failed to create PeerConnectionFactory\n";
+      std::exit(-1);
+    }
+
+    webrtc::PeerConnectionInterface::RTCConfiguration config{};
+    config.sdp_semantics = webrtc::SdpSemantics::kUnifiedPlan;
+    webrtc::PeerConnectionInterface::IceServer ice_server{};
+    ice_server.uri = "stun:localhost:3478";
+    config.servers.push_back(ice_server);
+
+    const auto maybe_pc = pc_factory->CreatePeerConnectionOrError(
+        config, webrtc::PeerConnectionDependencies{this});
+
+    if (!maybe_pc.ok()) {
+      std::cerr << "Failed to create PeerConnection\n";
+      std::exit(-1);
+    }
+
+    peer_connection = std::move(maybe_pc.value());
+
+    server.init_asio();
+    server.set_message_handler(
+        websocketpp::lib::bind(&observer::on_message, this, ::_1, ::_2));
+
+    server.set_open_handler(
+        websocketpp::lib::bind(&observer::on_open, this, ::_1));
+
+    server.listen(9002);
+    server.start_accept();
+    server.run();
+  }
+
+ private:
+  server_type server{};
+  rtc::scoped_refptr<webrtc::PeerConnectionInterface> peer_connection{};
+  websocketpp::connection_hdl connection{};
+
+  void on_message(websocketpp::connection_hdl hdl, message_ptr message) {
+    if (hdl.lock() != connection.lock()) {
+      std::cerr << "Wrong socket!\n";
+      return;
+    }
+
+    if (message->get_opcode() != 1) {
+      std::cerr << "I don't know how to use this frame\n";
+      return;
+    }
+
+    std::cout << "Received: " << message->get_payload() << "\n";
+    auto payload = boost::json::parse(message->get_payload()).as_object();
+    auto offer = payload["offer"].as_object();
+    std::cout << "Offer: " << offer << "\n";
+
+    peer_connection->SetRemoteDescription(
+        this, webrtc::CreateSessionDescription(
+                  webrtc::SdpTypeFromString(offer["type"].as_string().c_str())
+                      .value(),
+                  offer["sdp"].as_string().c_str())
+                  .release());
+
+    peer_connection->CreateAnswer(
+        this, webrtc::PeerConnectionInterface::RTCOfferAnswerOptions{});
+  }
+
+  void on_open(websocketpp::connection_hdl hdl) {
+    std::cout << "Connection opened\n";
+    connection = server.get_con_from_hdl(hdl);
+  }
+
   void OnSignalingChange(
       webrtc::PeerConnectionInterface::SignalingState new_state) override {}
 
@@ -22,31 +117,40 @@ class Observer : public webrtc::PeerConnectionObserver {
 
   void OnIceCandidate(const webrtc::IceCandidateInterface* candidate) override {
   }
+
+  void OnSuccess(webrtc::SessionDescriptionInterface* desc) override {
+    std::cout << "Created local session description\n";
+    peer_connection->SetLocalDescription(this, desc);
+    boost::json::object data{};
+    data["type"] = webrtc::SdpTypeToString(desc->GetType());
+    std::string sdp{};
+    if (!desc->ToString(&sdp)) {
+      std::cerr << "Failed to serialize SDP\n";
+      return;
+    }
+
+    data["sdp"] = sdp;
+    boost::json::object msg{};
+    msg["answer"] = data;
+    server.send(connection, boost::json::serialize(msg),
+                websocketpp::frame::opcode::text);
+  }
+
+  void OnSuccess() override { std::cerr << "Succeeded!\n"; }
+
+  void OnFailure(webrtc::RTCError error) override {
+    std::cerr << "Failed: " << error.message() << "\n";
+  }
 };
+
 }  // namespace receiver
 
 int main() {
-  const auto signaling_thread = rtc::Thread::CreateWithSocketServer();
-  signaling_thread->Start();
-  const auto pc_factory = webrtc::CreatePeerConnectionFactory(
-      nullptr, nullptr, signaling_thread.get(), nullptr,
-      webrtc::CreateBuiltinAudioEncoderFactory(),
-      webrtc::CreateBuiltinAudioDecoderFactory(),
-      webrtc::CreateBuiltinVideoEncoderFactory(),
-      webrtc::CreateBuiltinVideoDecoderFactory(), nullptr, nullptr);
-
-  if (!pc_factory) {
-    std::cerr << "Failed to create PeerConnectionFactory\n";
+  const auto observer = rtc::make_ref_counted<receiver::observer>();
+  try {
+    observer->start_signal_server();
+  } catch (const websocketpp::exception& error) {
+    std::cerr << error.what() << "\n";
     return -1;
   }
-
-  webrtc::PeerConnectionInterface::RTCConfiguration config{};
-  config.sdp_semantics = webrtc::SdpSemantics::kUnifiedPlan;
-  webrtc::PeerConnectionInterface::IceServer server{};
-  server.uri = "stun:localhost:3478";
-  config.servers.push_back(server);
-
-  receiver::Observer observer{};
-  const auto maybe_pc = pc_factory->CreatePeerConnectionOrError(
-      config, webrtc::PeerConnectionDependencies{&observer});
 }
