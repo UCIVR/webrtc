8b6125486251b049c497439b5d790733c93b54e6
David Detweiler <daviddetweiler1@gmail.com>
2023-05-04 08:16:26 -0700

    Checkpoint

--- examples/libconductor/conductor.cc
+++ examples/libconductor/conductor.cc
@@ -5,6 +5,7 @@
 
 #include <api/create_peerconnection_factory.h>
 #include <api/peer_connection_interface.h>
+#include <common_video/libyuv/include/webrtc_libyuv.h>
 #include <rtc_base/thread.h>
 
 #include <boost/json.hpp>
@@ -29,13 +30,14 @@ using websocketpp::lib::placeholders::_2;
 namespace conductor {
 class observer : public webrtc::PeerConnectionObserver,
                  public webrtc::SetSessionDescriptionObserver,
-                 public webrtc::CreateSessionDescriptionObserver {
+                 public webrtc::CreateSessionDescriptionObserver,
+                 public rtc::VideoSinkInterface<webrtc::VideoFrame> {
  public:
   template <typename... types>
   void log_wrapper(bool is_error, types&&... arguments) {
     std::wstringstream stream{};
     ((stream << arguments), ...);
-    logger_impl(is_error, stream.str().c_str());
+    logger_impl(client, is_error, stream.str().c_str());
   }
 
   template <typename... types>
@@ -48,43 +50,49 @@ class observer : public webrtc::PeerConnectionObserver,
     log_wrapper(true, arguments...);
   }
 
-  observer(log_function* logger_impl) : observer{} {
+  observer(void* client, log_function* logger_impl, on_video* on_video_impl)
+      : observer{} {
+    this->client = client;
     this->logger_impl = logger_impl;
+    this->on_video_impl = on_video_impl;
   }
 
   void start_signal_server() {
-    signal_thread = rtc::Thread::CreateWithSocketServer();
-    signal_thread->Start();
-    const auto pc_factory = webrtc::CreatePeerConnectionFactory(
-        nullptr, nullptr, signal_thread.get(), nullptr,
-        webrtc::CreateBuiltinAudioEncoderFactory(),
-        webrtc::CreateBuiltinAudioDecoderFactory(),
-        webrtc::CreateBuiltinVideoEncoderFactory(),
-        webrtc::CreateBuiltinVideoDecoderFactory(), nullptr, nullptr);
-
-    if (!pc_factory) {
-      log_error("Failed to create PeerConnectionFactory\n");
-      std::exit(-1);
-    }
-
-    webrtc::PeerConnectionInterface::RTCConfiguration config{};
-    config.sdp_semantics = webrtc::SdpSemantics::kUnifiedPlan;
-    webrtc::PeerConnectionInterface::IceServer turner{};
-    turner.uri = "turn:54.200.166.206:3478?transport=tcp";
-    turner.username = "user";
-    turner.password = "root";
-    config.servers.emplace_back(std::move(turner));
-
-    const auto maybe_pc = pc_factory->CreatePeerConnectionOrError(
-        config, webrtc::PeerConnectionDependencies{this});
-
-    if (!maybe_pc.ok()) {
-      log_error(L"Failed to create PeerConnection\n");
-      std::exit(-1);
-    }
-
-    peer_connection = std::move(maybe_pc.value());
     server_thread = std::thread{[this] {
+      signal_thread = rtc::Thread::CreateWithSocketServer();
+      worker_thread = rtc::Thread::CreateWithSocketServer();
+      signal_thread->Start();
+      worker_thread->Start();
+      const auto pc_factory = webrtc::CreatePeerConnectionFactory(
+          nullptr, worker_thread.get(), signal_thread.get(), nullptr,
+          webrtc::CreateBuiltinAudioEncoderFactory(),
+          webrtc::CreateBuiltinAudioDecoderFactory(),
+          webrtc::CreateBuiltinVideoEncoderFactory(),
+          webrtc::CreateBuiltinVideoDecoderFactory(), nullptr, nullptr);
+
+      if (!pc_factory) {
+        log_error("Failed to create PeerConnectionFactory\n");
+        std::exit(-1);
+      }
+
+      webrtc::PeerConnectionInterface::RTCConfiguration config{};
+      config.sdp_semantics = webrtc::SdpSemantics::kUnifiedPlan;
+      webrtc::PeerConnectionInterface::IceServer turner{};
+      turner.uri = "turn:54.200.166.206:3478?transport=tcp";
+      turner.username = "user";
+      turner.password = "root";
+      config.servers.emplace_back(std::move(turner));
+
+      const auto maybe_pc = pc_factory->CreatePeerConnectionOrError(
+          config, webrtc::PeerConnectionDependencies{this});
+
+      if (!maybe_pc.ok()) {
+        log_error(L"Failed to create PeerConnection\n");
+        std::exit(-1);
+      }
+
+      peer_connection = std::move(maybe_pc.value());
+
       server.init_asio();
       server.set_message_handler(
           websocketpp::lib::bind(&observer::on_message, this, ::_1, ::_2));
@@ -109,22 +117,29 @@ class observer : public webrtc::PeerConnectionObserver,
 
     server_thread.join();
     if (peer_connection) {
-      signal_thread->BlockingCall([this] {peer_connection->Close();});
+      signal_thread->BlockingCall([this] { peer_connection->Close(); });
     }
 
     log("Destructor completed");
   }
 
  private:
+  void* client{};
   std::unique_ptr<rtc::Thread> signal_thread{};
+  std::unique_ptr<rtc::Thread> worker_thread{};
   log_function* logger_impl{};
+  on_video* on_video_impl{};
   std::thread server_thread{};
   server_type server{};
   rtc::scoped_refptr<webrtc::PeerConnectionInterface> peer_connection{};
-  rtc::scoped_refptr<webrtc::MediaStreamTrackInterface> track{};
+  rtc::scoped_refptr<webrtc::VideoTrackInterface> track{};
   server_type::connection_ptr connection{};
+  std::uint64_t frames{};
+  std::mutex big_lock{};
 
   void on_message(websocketpp::connection_hdl hdl, message_ptr message) {
+    // std::lock_guard guard{big_lock};
+
     if (hdl.lock() != connection) {
       log_error("Wrong socket!\n");
       return;
@@ -175,6 +190,7 @@ class observer : public webrtc::PeerConnectionObserver,
   }
 
   void on_open(websocketpp::connection_hdl hdl) {
+    // std::lock_guard guard{big_lock};
     log("Connection opened\n");
     if (connection) {
       connection->close(websocketpp::close::status::going_away,
@@ -290,6 +306,7 @@ class observer : public webrtc::PeerConnectionObserver,
   }
 
   void OnSuccess(webrtc::SessionDescriptionInterface* desc) override {
+    // std::lock_guard guard{big_lock};
     log("Created local session description\n");
     peer_connection->SetLocalDescription(this, desc);
     boost::json::object data{};
@@ -315,18 +332,58 @@ class observer : public webrtc::PeerConnectionObserver,
 
   void OnTrack(rtc::scoped_refptr<webrtc::RtpTransceiverInterface> transceiver)
       override {
+    // std::lock_guard guard{big_lock};
+    if (track) {
+      log_error("Expected only one track!");
+    }
+
     log("Added track of type: ",
         cricket::MediaTypeToString(transceiver->media_type()).c_str(), "\n");
-    track = transceiver->receiver()->track();
-    if (track->enabled())
+    auto maybe_track = transceiver->receiver()->track();
+    if (maybe_track->enabled())
       log("Track is enabled\n");
+
+    if (maybe_track->kind() != track->kVideoKind) {
+      log_error("Not the expected video track!");
+    }
+
+    track = static_cast<webrtc::VideoTrackInterface*>(maybe_track.get());
+    track->AddOrUpdateSink(this, {});
+  }
+
+  void OnFrame(const webrtc::VideoFrame& frame) override {
+    // std::lock_guard guard{big_lock};
+    ++frames;
+    if (frames % 600 == 0)
+      log("Another 600 frames were received...");
+
+    on_video_impl(
+        client, frame.width(), frame.height(),
+        [](void* raw_frame, std::uint8_t* buffer, std::uint64_t size) {
+          const auto& frame =
+              *reinterpret_cast<const webrtc::VideoFrame*>(raw_frame);
+
+          const auto expected_size = webrtc::CalcBufferSize(
+              webrtc::VideoType::kARGB, frame.width(), frame.height());
+
+          if (expected_size > size)
+            return false;
+
+          webrtc::ConvertFromI420(frame, webrtc::VideoType::kARGB, 0, buffer);
+
+          return true;
+        },
+        const_cast<void*>(static_cast<const void*>(&frame)));
   }
 
   observer() = default;
 };
 
-observer_handle::observer_handle(log_function* logger_impl)
-    : impl{rtc::make_ref_counted<observer>(logger_impl).release()} {}
+observer_handle::observer_handle(void* client,
+                                 log_function* logger_impl,
+                                 on_video* on_video_impl)
+    : impl{rtc::make_ref_counted<observer>(client, logger_impl, on_video_impl)
+               .release()} {}
 
 observer_handle::observer_handle(observer_handle&& other) : impl{} {
   using namespace std;
--- examples/libconductor/conductor.h
+++ examples/libconductor/conductor.h
@@ -6,14 +6,18 @@
 #define LIBCONDUCTOR_API __declspec(dllimport)
 #endif
 
+#include <cstdint>
+
 namespace conductor {
 class observer;
 
-using log_function = void(bool is_error, const wchar_t* string);
+using log_function = void(void*, bool is_error, const wchar_t* string);
+using video_callback = bool(void*, std::uint8_t*, std::uint64_t);
+using on_video = void(void*, std::uint64_t width, std::uint64_t height, video_callback* callback, void* data);
 
 class LIBCONDUCTOR_API observer_handle {
  public:
-  observer_handle(log_function* logger_impl);
+  observer_handle(void* client, log_function* logger_impl, on_video* video_event);
   ~observer_handle();
   observer_handle(observer_handle&) = delete;
   observer_handle(observer_handle&&);
